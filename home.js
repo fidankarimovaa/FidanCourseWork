567492d85d87f398d0c8e30c774bd45aac260247
fidan2004 <fidankk@code.edu.az>
2024-05-15 23:11:46 +0400

    commit changes

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
-function showMessagePage() {
-    window.location.href = "message.html";
-}
+document.getElementById("redirectButton").onclick = function() {
+    var form = document.getElementById("reservationForm");
+
+   
+    if (form.checkValidity()) {
+        window.location.href = "message.html";
+    } else {
+       
+        alert("Please fill out all required fields.");
+    }
+};
